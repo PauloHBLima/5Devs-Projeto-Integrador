e022070e1055be6dc826f870170073fb28a0f21c
Marcel Henrique <marcelsimoesalexandre@outlook.com>
2022-08-14 22:20:53 -0300

    Migrates

--- Models/User.js
+++ Models/User.js
@@ -1,43 +1,42 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User',{
+    const User = sequelize.define('User', {
         id: {
             type: DataTypes.INTEGER(10),
             primaryKey: true,
             autoIncrement: true,
             allowNull: false
-          },
-          name: {
+        },
+        name: {
             type: DataTypes.STRING(200),
             allowNull: false,
-          },
-    
-          email: {
+        },
+
+        email: {
             type: DataTypes.STRING(200),
             allowNull: false,
             unique: true
-          },
-          password: {
+        },
+        password: {
             type: DataTypes.STRING(300),
             allowNull: false,
-          },
+        },
         cep: {
-          type: DataTypes.STRING(200),
-          allowNull: true,
+            type: DataTypes.STRING(200),
+            allowNull: true,
         },
-        address_number: {
-          type: DataTypes.STRING(20),
-          allowNull: true,
+        addressNumber: {
+            type: DataTypes.STRING(20),
+            allowNull: true,
+            field: 'address_number'
         },
-        
-        is_admin: {
-          type: DataTypes.TINYINT,
-          allowNull: false,
+
+        isAdmin: {
+            type: DataTypes.TINYINT,
+            allowNull: false,
+            field: 'is_admin'
         }
-           
-        },{
-            tableName: 'users',
-            timestamps: true,
-        });
-    return User;
+
+    })
+
+    return User
 }
-       
