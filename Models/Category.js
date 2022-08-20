cb210ad2adf29b4def15020e3dbea0a286fe346c
Paulo Henrique <ph.blima30@gmail.com>
2022-08-20 12:05:51 -0300

    model categoria

---
+++ Models/Category.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+       
+        id: {
+            type: DataTypes.INTEGER(10),
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+          },
+    
+          name: {
+            type: DataTypes.STRING(200),
+            allowNull: false
+          }
+    
+        }, {
+            tableName: "categories",
+            timestamps: true
+        });
+    }
